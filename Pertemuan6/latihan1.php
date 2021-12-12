46154493b493bc8f5469be10aa0e0a842044ec0d
TubagusR <tubagus@ezeelink.co.id>
2021-12-12 11:43:31 +0700

    push new dir

---
+++ Pertemuan6/latihan1.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$conn = mysqli_connect("$servername","$username","$password");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "Connected successfully";
+}
+
+?>
